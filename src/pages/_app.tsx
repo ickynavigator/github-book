616bc82c436421c9e02830532f9f11ee4783a6e3
Obi Fortune <gabrielobi.of@gmail.com>
2022-12-30 14:35:00 +0100

    added source code

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,19 @@
-import { MantineProvider } from '@mantine/core';
+import { Anchor, Center, MantineProvider } from '@mantine/core';
 import { type AppType } from 'next/app';
 
 import { trpc } from '../utils/trpc';
 
+const github = 'https://github.com/ickynavigator/github-book';
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <MantineProvider withGlobalStyles withNormalizeCSS>
       <Component {...pageProps} />
+
+      <Center>
+        <Anchor href={github} target="_blank">
+          SOURCE CODE
+        </Anchor>
+      </Center>
     </MantineProvider>
   );
 };
