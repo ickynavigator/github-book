512d60696f857b583f20c538544f6e9326287467
Obi Fortune <gabrielobi.of@gmail.com>
2022-12-30 18:03:54 +0100

    made github token optional

--- src/env/schema.mjs
+++ src/env/schema.mjs
@@ -7,7 +7,7 @@ import { z } from 'zod';
  */
 export const serverSchema = z.object({
   NODE_ENV: z.enum(['development', 'test', 'production']),
-  GITHUB_TOKEN: z.string(),
+  GITHUB_TOKEN: z.string().optional(),
 });
 
 /**
