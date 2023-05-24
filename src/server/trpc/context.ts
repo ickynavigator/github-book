12fcfd4d76f6bfa4596543144329a99fb5efc528
Obi Fortune <gabrielobi.of@gmail.com>
2023-05-24 22:51:02 +0100

    fixed build warnings

--- src/server/trpc/context.ts
+++ src/server/trpc/context.ts
@@ -1,5 +1,5 @@
-import { type inferAsyncReturnType } from "@trpc/server";
-import { type CreateNextContextOptions } from "@trpc/server/adapters/next";
+import { type inferAsyncReturnType } from '@trpc/server';
+import { type CreateNextContextOptions } from '@trpc/server/adapters/next';
 
 /**
  * Replace this with an object if you want to pass things to createContextInner
@@ -11,6 +11,7 @@ type CreateContextOptions = Record<string, never>;
  * - trpc's `createSSGHelpers` where we don't have req/res
  * @see https://create.t3.gg/en/usage/trpc#-servertrpccontextts
  **/
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const createContextInner = async (opts: CreateContextOptions) => {
   return {};
 };
@@ -19,6 +20,7 @@ export const createContextInner = async (opts: CreateContextOptions) => {
  * This is the actual context you'll use in your router
  * @link https://trpc.io/docs/context
  **/
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const createContext = async (opts: CreateNextContextOptions) => {
   return await createContextInner({});
 };
